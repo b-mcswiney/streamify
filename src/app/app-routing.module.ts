6748b031ec666bc469ea56ba77384e6e5a284a0c
b-mcswiney <brooklynmcswiney@gmail.com>
2024-12-23 15:53:35 +0000

    last push was from wrong directory. Current push corrects this

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { MoviesComponent } from './movies/movies.component';
 import { LoginComponent } from './login/login.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { MyreviewsComponent } from './myreviews/myreviews.component';
+import { authGuard } from './service/auth.guard';
 
 const routes: Routes = [
   {path: "", redirectTo:'movies', pathMatch:"full"},
   {path: "movies", component:MoviesComponent},
   {path: "login", component:LoginComponent},
+  {path: "myreviews", component:MyreviewsComponent, canActivate:[authGuard]},
   {path: '**', component:PagenotfoundComponent}
 ];
 
